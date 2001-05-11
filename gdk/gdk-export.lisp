7ab3c23fc84d0cb4eb60dfca5e17744bc2ef1327
espen <espen>
2001-05-11 16:22:36 +0000

    Removed gdkenums.lisp from and added gdkevents.lisp to list of file symbols are exported from

--- gdk/gdk-export.lisp
+++ gdk/gdk-export.lisp
@@ -1,6 +1,6 @@
 (in-package "GDK")
 
 ;;; Autogenerating exported symbols
-(export-from-file #p"clg:gdk;gdkenums.lisp")
+(export-from-file #p"clg:gdk;gdkevents.lisp")
 (export-from-file #p"clg:gdk;gdktypes.lisp")
 (export-from-file #p"clg:gdk;gdk.lisp")
