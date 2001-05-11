6cb55200f7d6e0107a64a7c774f0a392e41ad689
espen <espen>
2001-05-11 16:23:15 +0000

    Removed shadow import of DEFTYPE

--- gdk/gdk-package.lisp
+++ gdk/gdk-package.lisp
@@ -1,7 +1,6 @@
 (defpackage "GDK"
   (:use "GLIB" "COMMON-LISP" "AUTOEXPORT")
   (:shadowing-import-from "PCL"
-   "CLASS-NAME" "CLASS-OF" "FIND-CLASS")
-  (:shadowing-import-from "GLIB" "DEFTYPE"))
+   "CLASS-NAME" "CLASS-OF" "FIND-CLASS"))
 
 
