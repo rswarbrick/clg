e86a5b8ff6c0865093e846993d598bcee90c96ba
espen <espen>
2004-10-27 18:44:53 +0000

    gdk-export.lisp renamed

---
+++ gdk/export.lisp
@@ -0,0 +1,6 @@
+(in-package "GDK")
+
+;;; Autogenerating exported symbols
+(export-from-file #p"clg:gdk;gdkevents.lisp")
+(export-from-file #p"clg:gdk;gdktypes.lisp")
+(export-from-file #p"clg:gdk;gdk.lisp")
