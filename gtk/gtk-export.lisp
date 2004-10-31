62fad69bd89e77f8cffec440109ffac498934863
espen <espen>
2004-10-31 11:57:19 +0000

    Renamed to export.lisp

--- gtk/gtk-export.lisp
+++
@@ -1,8 +0,0 @@
-(in-package "GTK")
-
-;;; Autogenerating exported symbols
-(export-from-file #p"clg:gtk;gtktypes.lisp")
-(export-from-file #p"clg:gtk;gtkwidget.lisp")
-(export-from-file #p"clg:gtk;gtkcontainer.lisp")
-(export-from-file #p"clg:gtk;gtk.lisp")
-(export-from-file #p"clg:gtk;gtkutils.lisp")
