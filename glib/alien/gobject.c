aa120a458066e5aba7ca819d038847f2e57e5c92
espen <espen>
2006-02-19 19:07:49 +0000

    Removed unused code

--- glib/alien/gobject.c
+++ glib/alien/gobject.c
@@ -21,25 +21,9 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-/* $Id: gobject.c,v 1.3 2005-04-23 16:48:51 espen Exp $ */
+/* $Id: gobject.c,v 1.4 2006-02-19 19:07:49 espen Exp $ */
 
 #include <glib-object.h>
-
-gpointer g_object_newvv (GType object_type, guint n_parameters,
-			 guchar* names[], GValue* values[])
-{
-  GParameter parameters[n_parameters];
-  int i;
-
-  for (i = 0; i < n_parameters; i++) {
-    parameters[i].name = names[i];
-    g_value_init (&parameters[i].value, G_VALUE_TYPE (values[i]));
-    g_value_copy (values[i], &parameters[i].value);
-  }
-
-  return g_object_newv (object_type, n_parameters, &parameters);
-}
-
 
 guint size_of_gvalue ()
 {
