dc3ddae06d0fe207ef6702f29bbd1d5790b3f232
espen <espen>
2007-06-19 11:39:26 +0000

    Added proper protection for multi threaded event handling

--- hello-world.lisp
+++ hello-world.lisp
@@ -1,18 +1,19 @@
 (gtk:clg-init)
 
-(make-instance 'gtk:window
-; :type :toplevel
- :title "Test"
- :border-width 5
- :visible t :show-children t
- :child (make-instance 'gtk:button
-	 :label "Hello World!"
-	 :signal (list 'clicked
-		       #'(lambda ()
-			   (write-line "Hello World!"))))
- :signal (list 'delete-event
-	       #'(lambda (event)
-		   (declare (ignore event))
-		   (write-line "Destroying window")
-		   nil ; Returning NIL generates a destroy event
-		   )))
+(gtk:within-main-loop
+  (make-instance 'gtk:window
+;   :type :toplevel
+   :title "Test"
+   :border-width 5
+   :visible t :show-children t
+   :child (make-instance 'gtk:button
+	   :label "Hello World!"
+	   :signal (list 'clicked
+		    #'(lambda ()
+			(write-line "Hello World!"))))
+   :signal (list 'delete-event
+	    #'(lambda (event)
+		(declare (ignore event))
+		(write-line "Destroying window")
+		nil ; Returning NIL generates a destroy event
+		))))
