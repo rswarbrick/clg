51c39c68d65b055852174fcf83441d90de0e7b05
espen <espen>
2008-11-19 07:53:50 +0000

    Fixed return type for COMBO-BOX-GET-ACTIVE-TEXT

--- gtk/gtk.lisp
+++ gtk/gtk.lisp
@@ -20,7 +20,7 @@
 ;; TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 ;; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-;; $Id: gtk.lisp,v 1.96 2008-11-04 20:18:08 espen Exp $
+;; $Id: gtk.lisp,v 1.97 2008-11-19 07:53:50 espen Exp $
 
 
 (in-package "GTK")
@@ -788,7 +788,7 @@
   (text string))
 
 #?(pkg-exists-p "gtk+-2.0" :atleast-version "2.6.0")
-(defbinding combo-box-get-active-text () string
+(defbinding combo-box-get-active-text () (or string null)
   (combo-box combo-box))
 
 (defbinding combo-box-popup () nil
